ddf788630a2acb05aca7f3060965dd7c6c06f357
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-04-01 12:31:41 +0530

    update enums

--- src/main/java/com/notes/enums/EnumDemo.java
+++ src/main/java/com/notes/enums/EnumDemo.java
@@ -4,13 +4,16 @@ public class EnumDemo {
 
 	public static void main(String[] args) {
 		
-		Day d1 = Day.MONDAY;
-		System.out.println("Test: " + d1);
+		Day day1 = Day.MONDAY;
+		System.out.println("Enum: " + day1);
+		System.out.println("Enum name: " + day1.name());
+		System.out.println("Enum message: " + day1.getMessage());
 
 	    // Loop through all enum values and print their message
 		System.out.println();
 	    for (Day day : Day.values()) {
-	        System.out.println(day + ": " + day.getMessage());
+	        // System.out.println(day + ": " + day.getMessage());
+	    	System.out.println(day.name() + ": " + day.getMessage());
 	    }
 	    
         // Loop through all enum values and display their info
