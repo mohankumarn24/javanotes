2d65636a49ff8f5b5eead45a1067e74494aa516e
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-04-15 11:29:28 +0530

    refactor

--- src/main/java/com/notes/multithreading/daemonthreads/DaemonThreadDemo.java
+++ src/main/java/com/notes/multithreading/daemonthreads/DaemonThreadDemo.java
@@ -7,8 +7,8 @@ public class DaemonThreadDemo {
 		Runtime runtime = Runtime.getRuntime();				// shutdownhook. Invoked automatically on JVM shutdown
 		runtime.addShutdownHook(new ShutdownHook());
 		
-		Thread t1 = new MyThread();	// daemon thread. runs forever
-		Thread t2 = new MyThread();	// user thread. runs for 5 sec and terminates. Then, it kills all daemon threads and invokes shutdownhook
+		Thread t1 = new MyThread();							// daemon thread. runs forever
+		Thread t2 = new MyThread();							// user thread. runs for 5 sec and terminates. Then, it kills all daemon threads and invokes shutdownhook
 		
 		t1.setDaemon(true);
 		
