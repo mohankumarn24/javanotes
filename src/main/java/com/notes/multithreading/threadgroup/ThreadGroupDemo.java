667b0f5ffbcd19813e3d8e186f1536c82cb74e74
Mohan Kumar N <mohankumar.n24@outlook.com>
2025-05-20 17:36:54 +0530

    refactor

--- src/main/java/com/notes/multithreading/threadgroup/ThreadGroupDemo.java
+++ src/main/java/com/notes/multithreading/threadgroup/ThreadGroupDemo.java
@@ -5,20 +5,20 @@ public class ThreadGroupDemo {
 	public static void main(String[] args) {
 		
 		Runnable runnable = new ThreadGroupWorkerThread();
-		ThreadGroup tg1 = new ThreadGroup("Parent ThreadGroup");
+		ThreadGroup threadGroup = new ThreadGroup("Parent ThreadGroup");
 		
-		Thread t1 = new Thread(tg1, runnable, "one");    
-		Thread t2 = new Thread(tg1, runnable, "two");    
-		Thread t3 = new Thread(tg1, runnable, "three"); 
+		Thread t1 = new Thread(threadGroup, runnable, "one");    
+		Thread t2 = new Thread(threadGroup, runnable, "two");    
+		Thread t3 = new Thread(threadGroup, runnable, "three"); 
 		
 		t1.start();
 		t2.start();
 		t3.start();
 		
-        System.out.println("\nThread Group Name: " + tg1.getName());
+        System.out.println("\nThread Group Name: " + threadGroup.getName());
         System.out.println("\nThread Group List: ");
-        tg1.list();
-        System.out.println("\nThe total number of active threads are: " + tg1.activeCount()); 
-        System.out.println("\nThe total number of active thread groups are: " + tg1.activeGroupCount());  
+        threadGroup.list();
+        System.out.println("\nThe total number of active threads are: " + threadGroup.activeCount()); 
+        System.out.println("\nThe total number of active thread groups are: " + threadGroup.activeGroupCount());  
 	}
 }
