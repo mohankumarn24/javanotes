c9b2361d80a3f5579168d9ae2186bccdf00186a7
mohankumarn24 <mohankumar.n24@outlook.com>
2025-11-25 14:32:57 +0530

    refactor for readability

--- src/main/java/com/notes/multithreading/daemonthreads/MyThread.java
+++ src/main/java/com/notes/multithreading/daemonthreads/MyThread.java
@@ -8,19 +8,11 @@ public class MyThread extends Thread {
 		if (Thread.currentThread().isDaemon()) {
 			while (true) {
 				System.out.println("Daemon thread running...");
-				try {
-					Thread.sleep(1000);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
+				try { Thread.sleep(1000); } catch (InterruptedException e) { e.printStackTrace(); }
 			}
 		} else {
 			System.out.println("@@ User thread sleeps for 5 seconds...");
-			try {
-				Thread.sleep(5000);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
+			try { Thread.sleep(5000); } catch (InterruptedException e) { e.printStackTrace(); }
 			System.out.println("@@ User thread terminated automatically after 5 seconds");
 		}
 	}
