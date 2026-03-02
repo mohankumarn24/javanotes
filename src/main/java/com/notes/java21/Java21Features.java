423fc1dc2ffe47cc38996ead836767592499949d
mohankumarn24 <mohankumar.n24@outlook.com>
2026-03-02 20:11:35 +0530

    update

---
+++ src/main/java/com/notes/java21/Java21Features.java
@@ -0,0 +1,10 @@
+package com.notes.java21;
+
+public class Java21Features {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/main/java/com/notes/java25/Java25Features.java
@@ -0,0 +1,9 @@
+package com.notes.java25;
+
+public class Java25Features {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
